12dba5c5574b5657186cd870a09ec66f9b32d1df
Ludvig Källqvist Nygren <lukecastellan165@gmail.com>
2024-01-30 12:37:14 +0100

    fix: comments and small useability update

--- symlinks/nvim/lua/aabrupt/settings.lua
+++ symlinks/nvim/lua/aabrupt/settings.lua
@@ -1,26 +1,36 @@
+-- Search character cases
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
+-- Indentation
 vim.o.tabstop = 8
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.breakindent = true
 
+-- Timings
 vim.o.updatetime = 250
 vim.o.timoutlen = 300
 
+-- Left column
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.wo.signcolumn = 'yes'
 
+-- Undo and copy/paste
 vim.o.undofile = true
 vim.o.clipboard = 'unnamedplus'
 
-vim.o.termguicolors = true
+-- Mouse
 vim.o.completeopt = 'menuone,noselect'
-vim.o.mouse = 'a'
+vim.o.mouse = ''
 vim.o.hlsearch = false
 
+-- Netrw
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+-- GUI
+vim.o.termguicolors = true
+vim.o.colorcolumn = "80,120"
